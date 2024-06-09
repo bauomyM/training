678b1176aa8e8c252836d1518bd49489bf08bdaf
laratawbe <115413984+laratawbe@users.noreply.github.com>
2024-06-10 02:32:31 +0300

    still null

--- src/main/kotlin/com/example/demo/graphql/UserMutationResolver.kt
+++ src/main/kotlin/com/example/demo/graphql/UserMutationResolver.kt
@@ -2,18 +2,24 @@ package com.example.demo.graphql
 
 import com.coxautodev.graphql.tools.GraphQLMutationResolver
 import com.example.demo.User
-import com.example.demo.UserRepository
+import com.example.demo.kafka.KafkaMessageProducer
 import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.MutationMapping
 import org.springframework.stereotype.Controller
-import com.example.demo.kafka.KafkaMessageProducer
 
 @Controller
 class UserMutationResolver(private val kafkaMessageProducer: KafkaMessageProducer) : GraphQLMutationResolver {
 
-    fun createUser(@Argument name: String,@Argument email: String, @Argument gender: String,@Argument archived: Boolean?, @Argument city: Array<String>?): User {
+    @MutationMapping
+    fun createUser(
+        @Argument name: String,
+        @Argument email: String,
+        @Argument gender: String,
+        @Argument archived: Boolean? = null,
+        @Argument city: Array<String>? = null
+    ): String {
         val user = User(name = name, email = email, gender = gender, archived = archived, city = city)
         kafkaMessageProducer.sendMessage("user-create", user)
-        return user
+        return "User creation request received. Processing..."
     }
 }
--- src/main/kotlin/com/example/demo/kafka/KafkaConsumerConfig.kt
+++ src/main/kotlin/com/example/demo/kafka/KafkaConsumerConfig.kt
@@ -1,5 +1,6 @@
 package com.example.demo.kafka
 
+import com.example.demo.User
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.springframework.context.annotation.Bean
@@ -22,7 +23,10 @@ class KafkaConsumerConfig {
         configProps[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
         configProps[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = ErrorHandlingDeserializer::class.java
         configProps[ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS] = JsonDeserializer::class.java.name
+        configProps[JsonDeserializer.VALUE_DEFAULT_TYPE] = User::class.java.name
         configProps[JsonDeserializer.TRUSTED_PACKAGES] = "*"
+        configProps[JsonDeserializer.USE_TYPE_INFO_HEADERS] = false
+
         return DefaultKafkaConsumerFactory(configProps)
     }
 
