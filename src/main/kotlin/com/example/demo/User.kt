ca97e58038eb863006762d1c7c73630e614c99ac
laratawbe <115413984+laratawbe@users.noreply.github.com>
2024-06-10 02:56:51 +0300

    trial 2

--- src/main/kotlin/com/example/demo/User.kt
+++ src/main/kotlin/com/example/demo/User.kt
@@ -2,13 +2,14 @@ package com.example.demo
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
+import java.io.Serializable
 
 @Document(collection = "users")
 data class User (
-    @Id val id: String?= null,
-            val name: String,
-            val email: String,
-            val gender: String,
-            val archived: Boolean?= null,
-            val city: Array<String>?=null
-)
+        @Id val id: String?= null,
+        val name: String,
+        val email: String,
+        val gender: String,
+        val archived: Boolean?= null,
+        val city: Array<String>?=null
+) : Serializable
--- src/main/kotlin/com/example/demo/kafka/KafkaMessageConsumer.kt
+++ src/main/kotlin/com/example/demo/kafka/KafkaMessageConsumer.kt
@@ -3,13 +3,14 @@ package com.example.demo.kafka
 import com.example.demo.User
 import com.example.demo.UserRepository
 import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.messaging.handler.annotation.Payload
 import org.springframework.stereotype.Service
 
 @Service
 class KafkaMessageConsumer(private val userRepository: UserRepository) {
 
     @KafkaListener(topics = ["user-create"], groupId = "group_id")
-    fun consume(user: User) {
+    fun consume(@Payload user: User) {
         userRepository.save(user)
     }
 }
