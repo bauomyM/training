513345e3bef56e1aa545e1e13e69c6c228ae200a
Mohammad Adel Alsoheil <alsoheilm@gmail.com>
2024-07-03 01:29:09 +0300

    JSON serialization error in redis

--- src/main/kotlin/com/example/demo/Service/CarService.kt
+++ src/main/kotlin/com/example/demo/Service/CarService.kt
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.stereotype.Service
 
-
+// serialization error encountered
 
 @Service
 class CarService(val carRepository: CarRepository,private val redisTemplate: RedisTemplate<String, Any>) {
