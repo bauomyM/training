2d431e69f029bf97095ce39d98dba3f3a24157b3
laratawbe <115413984+laratawbe@users.noreply.github.com>
2024-07-18 12:01:32 +0300

    added mocks to testing functions

--- src/test/kotlin/com/example/demo/Service/CarServiceTest.kt
+++ src/test/kotlin/com/example/demo/Service/CarServiceTest.kt
@@ -1,12 +1,30 @@
 package com.example.demo.Service
 
+import com.example.demo.Repo.CarRepository
+import com.example.demo.Resolvers.CarResolver
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.mockito.Mockito.mock
+import org.springframework.cache.CacheManager
+
 
 class CarServiceTest {
 
+    private lateinit var carRepository: CarRepository
+    private lateinit var carResolver: CarResolver
+    private lateinit var cacheManager: CacheManager
+    private lateinit var carService: CarService
 
+    @BeforeEach
+    fun setup() {
+        carRepository = mock(CarRepository::class.java)
+        carResolver = mock(CarResolver::class.java)
+        cacheManager = mock(CacheManager::class.java)
+        carService = CarService(carRepository, carResolver)
+        carService.cacheManager = cacheManager
+    }
     @Test
     fun findCars() {
     }
